ea1720cbe976abf127990c476b37b5db090b9095
shadrach <oforkansi.shadrach@gmail.com>
2021-10-26 10:34:41 +0100

    startup modified

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,10 +1,11 @@
-cd backend
 pip install -r requirements.txt
-
+cd -
 if [[ -f ./.git/hooks/pre-commit ]]
 then
     echo "pre-commit already setup"
 else
+    cd backend
     pre-commit install
 fi
+cd backend
 uvicorn main:app --reload
