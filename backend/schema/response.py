f0d657caa0e35ad22a35d7c6020a7684a7d276f9
shadrach <oforkansi.shadrach@gmail.com>
2021-11-17 03:41:56 +0100

    created success response

--- backend/schema/response.py
+++ backend/schema/response.py
@@ -22,29 +22,3 @@ class ResponseModel(BaseModel):
             dict: key-value pair of status, message and data
         """
         return ResponseModel(status="success", message=message, data=data).dict()
-
-    @staticmethod
-    def error(message, data=None):
-        """Provides an error response data
-
-        Args:
-            message (str): error message
-            data (dict, optional): [description]. Defaults to None.
-
-        Returns:
-            dict: key-value pair of status, message and data
-        """
-        return ResponseModel(status="error", message=message, data=data).dict()
-
-    @staticmethod
-    def fail(message, data=None):
-        """Provides a fail response data
-
-        Args:
-            message (str): descriptive failure message
-            data (dict, optional): key value pair of data returned. Defaults to None.
-
-        Returns:
-            dict: key-value pair of status, message and data
-        """
-        return ResponseModel(status="failed", message=message, data=data).dict()
