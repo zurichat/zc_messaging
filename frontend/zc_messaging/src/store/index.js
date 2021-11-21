2b7ed8c59a9ab7ab6daee8801159c0e85b53a419
Tunde Alabi <tundealabi4780@gmail.com>
2021-11-21 21:23:50 +0100

    prepare messaging client for FE workflows

--- frontend/zc_messaging/src/store/index.js
+++ frontend/zc_messaging/src/store/index.js
@@ -1,4 +1,13 @@
-import { createStore } from "redux"
-import reducers from "./reducers"
+import { configureStore } from "@reduxjs/toolkit"
+import logger from "redux-logger"
+import sampleReducer from "./reducers/templateSlice"
 
-export const store = createStore(reducers, {})
+export const store = configureStore({
+  reducer: {
+    sample: sampleReducer
+  },
+  middleware: getDefaultMiddleware =>
+    process.env.NODE_ENV !== "production"
+      ? getDefaultMiddleware().concat(logger)
+      : getDefaultMiddleware()
+})
