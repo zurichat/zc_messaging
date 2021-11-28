0a536c166fe0e791a3b7bfc76fa640764a2485e3
@zxenon_ <xenon.nemrod@gmail.com>
2021-11-28 22:14:09 +0100

    Fix import issue (#26)
    * Add gunicorn to requiremts.txt
    
    * Disable pylint rule C0103
    
    * Fix import issue

--- backend/endpoints/sync.py
+++ backend/endpoints/sync.py
@@ -1,29 +1,24 @@
+from datetime import datetime
+
 from bson.objectid import ObjectId
 from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import JSONResponse
-from schema.response import ResponseModel
-from schema.room import RoomType
-from utils.sidebar import sidebar
-from datetime import datetime
-from typing import Dict
-from fastapi import APIRouter, Request
-from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
-from starlette import status
-from starlette.responses import JSONResponse
-from backend.schema.custom import ObjId
-from backend.schema.response import ResponseModel
-from backend.schema.room import Role, RoomMember
-from backend.utils.db import DB
-from backend.utils.room_utils import ROOM_COLLECTION
-
-from schema.room import Room, RoomRequest, RoomType
+from schema.response import ResponseModel
+from schema.room import Role, RoomType
+from utils.db import DB
+from utils.room_utils import ROOM_COLLECTION
+from utils.sidebar import sidebar
 
 router = APIRouter()
 
+
 class InstallPayload(BaseModel):
+    """Installation payload model."""
+
     organisation_id: str
     user_id: str
+
 
 @router.post("/install")
 async def dm_install(payload: InstallPayload):
@@ -34,48 +29,40 @@ async def dm_install(payload: InstallPayload):
         "room_name": "general",
         "room_type": RoomType.CHANNEL,
         "room_members": {
-            payload.user_id: {
-                "role": Role.ADMIN,
-                "starred": False,
-                "closed": False
-            }
-        }, 
+            payload.user_id: {"role": Role.ADMIN, "starred": False, "closed": False}
+        },
         "created_at": str(datetime.utcnow()),
         "description": "",
         "topic": "",
         "is_private": False,
         "is_archived": False,
         "org_id": payload.organisation_id,
-        "created_by": payload.user_id
+        "created_by": payload.user_id,
     }
 
     dm = {
         "room_name": payload.user_id,
         "room_type": RoomType.DM,
         "room_members": {
-            payload.user_id: {
-                "role": Role.ADMIN,
-                "starred": False,
-                "closed": False
-            }
-        }, 
+            payload.user_id: {"role": Role.ADMIN, "starred": False, "closed": False}
+        },
         "created_at": str(datetime.utcnow()),
         "description": "",
         "topic": "",
         "is_private": False,
         "is_archived": False,
         "org_id": payload.organisation_id,
-        "created_by": payload.user_id
+        "created_by": payload.user_id,
     }
 
     channel_res = await DB.write(ROOM_COLLECTION, data=channel)
-    
-    if channel_res == None:
+
+    if channel_res is None:
         raise HTTPException(
             status_code=status.HTTP_424_FAILED_DEPENDENCY,
             detail="unable to remove room member",
-        )      
-    elif type(channel_res) is dict:
+        )
+    if isinstance(channel_res, dict):
         raise HTTPException(
             status_code=status.HTTP_424_FAILED_DEPENDENCY,
             detail="unable to remove room member",
@@ -83,21 +70,19 @@ async def dm_install(payload: InstallPayload):
 
     dm_res = await DB.write(ROOM_COLLECTION, data=dm)
 
-    if dm_res == None:
+    if dm_res is None:
         raise HTTPException(
             status_code=status.HTTP_424_FAILED_DEPENDENCY,
             detail="unable to remove room member",
-        )      
-    elif type(dm_res) is dict:
+        )
+    if isinstance(dm_res, dict) is dict:
         raise HTTPException(
             status_code=status.HTTP_424_FAILED_DEPENDENCY,
             detail="unable to remove room member",
         )
 
     return JSONResponse(
-        data=None,
-        message="installation successful",
-        status_code=status.HTTP_200_OK
+        data=None, message="installation successful", status_code=status.HTTP_200_OK
     )
 
 
