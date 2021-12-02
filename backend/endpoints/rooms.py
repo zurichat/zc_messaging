a0f8674c6a7ede56b3032802015d15fb0c18248c
AI-fae <73687704+AI-fae@users.noreply.github.com>
2021-12-02 15:22:59 -0600

    removing kinglighthill's commit

--- backend/endpoints/rooms.py
+++ backend/endpoints/rooms.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, BackgroundTasks, HTTPException, status
 from fastapi.responses import JSONResponse
 from schema.response import ResponseModel
-from schema.room import Role, Room, RoomMember, RoomRequest
+from schema.room import Room, RoomRequest
 from utils.db import DB
-from utils.room_utils import ROOM_COLLECTION, get_room
+from utils.room_utils import ROOM_COLLECTION
 from utils.sidebar import sidebar
 
 router = APIRouter()
@@ -60,82 +60,4 @@ async def create_room(
     raise HTTPException(
         status_code=status.HTTP_424_FAILED_DEPENDENCY,
         detail="unable to create room",
-    )
-
-
-@router.delete(
-    "/org/{org_id}/members/{member_id}/rooms/{room_id}",
-    response_model=ResponseModel,
-)
-async def remove_member(org_id: str, member_id: str, room_id: str, mem_id: str):
-    """Removes a member from a room.
-
-    Fetches the room which the member is removed from from the database collection
-    Pops the member being removed from the room's members dict
-    Updates the database collection with the new room
-    Returns the room dict if member was removed successfully
-
-    Args:
-        org_id (str): A unique identifier of an organisation
-        request: A pydantic schema that defines the room request parameters
-        member_id (str): A unique identifier of the member removing another member
-        room_id (str): A unique identifier of the room a member is being removed from
-        memb_id (str): A unique identifier of the member being removed from the room
-
-    Returns:
-        HTTP_200_OK (member removed from room): {room}
-    Raises
-        HTTP_404_NOT_FOUND: room or member not found
-        HTTP_424_FAILED_DEPENDENCY: member removal unsuccessful
-    """
-
-    room_obj = await get_room(org_id, room_id)
-
-    if not room_obj:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="room does not exist",
-        )
-
-    if member_id not in room_obj["room_members"]:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="user not a member of the room",
-        )
-
-    if member_id == mem_id:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="unable to remove room member",
-        )
-
-    member: RoomMember = room_obj["room_members"].get(member_id)
-
-    if member.role == Role.ADMIN:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="unable to remove room member",
-        )
-
-    drop_member = room_obj["room_members"].pop(mem_id, "not_found")
-
-    if drop_member != "not_found":
-        update_room = DB.update(ROOM_COLLECTION, room_obj["id"], room_obj)
-        if update_room is None or isinstance(update_room, dict):
-            raise HTTPException(
-                status_code=status.HTTP_424_FAILED_DEPENDENCY,
-                detail="unable to remove room member",
-            )
-
-        return JSONResponse(
-            content=ResponseModel.success(
-                data=room_obj.dict(),
-                message="member removed successfully from room",
-            ),
-            status_code=status.HTTP_200_OK,
-        )
-
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="user not a member of the room",
     )
