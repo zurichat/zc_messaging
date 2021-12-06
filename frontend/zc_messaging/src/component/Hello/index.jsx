d9ed62dcb188788401239a46526f92f6075066d5
tunde <61058516+tundealabi@users.noreply.github.com>
2021-12-06 14:35:51 +0100

    Feat/message board (#36)
    * route to and display messaging area
    
    * remove unused hello component

--- frontend/zc_messaging/src/component/Hello/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import "./hello.module.css"
-
-export default function Hello(props) {
-  return <h1 className="heading">Hello {props.text}</h1>
-}
--- frontend/zc_messaging/src/routes/index.jsx
+++ frontend/zc_messaging/src/routes/index.jsx
@@ -1,28 +1,22 @@
 import React from "react"
-import {
-  Switch,
-  Route,
-  BrowserRouter as Router,
-  Redirect
-} from "react-router-dom"
-import { ChannelBrowser, DmBrowser, MessageBoard, Threads } from "../pages"
+import { Switch, Route, Redirect } from "react-router-dom"
+import { ChannelBrowser, DmBrowser, MessagingBoard, Threads } from "../pages"
 
 /**
  * Main Routing for the zc messaging plugin.
  * All other routing must somehow be a descendant of this component.
  */
 const Routes = () => (
-  <Router>
-    <Switch>
-      <Route path="/browse-channels" component={ChannelBrowser} />
-      <Route path="/threads" component={Threads} />
-      <Route path="/all-dms" component={DmBrowser} />
-      <Route path="/:roomId" component={MessageBoard} />
-      <Route exact path="*">
-        <Redirect to="/all-dms" />
-      </Route>
-    </Switch>
-  </Router>
+  <Switch>
+    <Route path="/" exact component={MessagingBoard} />
+    <Route path="/browse-channels" component={ChannelBrowser} />
+    <Route path="/threads" component={Threads} />
+    <Route path="/all-dms" component={DmBrowser} />
+    <Route path="/:roomId" component={MessagingBoard} />
+    {/* <Route exact path="*">
+      <Redirect to="/all-dms" />
+    </Route> */}
+  </Switch>
 )
 
 export default Routes
