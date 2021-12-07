ac13219f36799db066db47309b02bc49efb3c570
tunde <61058516+tundealabi@users.noreply.github.com>
2021-12-07 20:19:51 +0100

    Update react router to v6 (#38)

--- frontend/zc_messaging/src/App.js
+++ frontend/zc_messaging/src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Routes from "./routes"
+import AppRoutes from "./routes"
 
 export default function App() {
-  return <Routes />
+  return <AppRoutes />
 }
--- frontend/zc_messaging/src/routes/index.jsx
+++ frontend/zc_messaging/src/routes/index.jsx
@@ -1,22 +1,24 @@
 import React from "react"
-import { Switch, Route, Redirect } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import { ThreadBar, UserProfileBar, ChannelDetails } from "../component"
 import { ChannelBrowser, DmBrowser, MessagingBoard, Threads } from "../pages"
 
 /**
  * Main Routing for the zc messaging plugin.
  * All other routing must somehow be a descendant of this component.
  */
-const Routes = () => (
-  <Switch>
-    <Route path="/" exact component={MessagingBoard} />
-    <Route path="/browse-channels" component={ChannelBrowser} />
-    <Route path="/threads" component={Threads} />
-    <Route path="/all-dms" component={DmBrowser} />
-    <Route path="/:roomId" component={MessagingBoard} />
-    {/* <Route exact path="*">
-      <Redirect to="/all-dms" />
-    </Route> */}
-  </Switch>
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<MessagingBoard />} />
+    <Route path="/:roomId" element={<MessagingBoard />}>
+      <Route path="thread/:threadId" element={<ThreadBar />} />
+      <Route path="member-profile/:memberId" element={<UserProfileBar />} />
+      <Route path="channel-details/:channelId" element={<ChannelDetails />} />
+    </Route>
+    <Route path="/browse-channels" element={<ChannelBrowser />} />
+    <Route path="/threads" element={<Threads />} />
+    <Route path="/all-dms" element={<DmBrowser />} />
+  </Routes>
 )
 
-export default Routes
+export default AppRoutes
