e6cb07829fa4c9ca6dd220026d52499775724be9
Tunde Alabi <tundealabi4780@gmail.com>
2021-12-08 13:56:52 +0100

    delete the pages/threads folder

--- frontend/zc_messaging/src/pages/Threads/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-const Threads = () => <h1>Threads</h1>
-
-export default Threads
--- frontend/zc_messaging/src/pages/index.js
+++ frontend/zc_messaging/src/pages/index.js
@@ -1,6 +1,5 @@
 import DmBrowser from "./dm_browser/DmBrowser"
 import MessagingBoard from "./message-board"
 import ChannelBrowser from "./channel-browser"
-import Threads from "./threads"
 
-export { MessagingBoard, DmBrowser, ChannelBrowser, Threads }
+export { MessagingBoard, DmBrowser, ChannelBrowser }
