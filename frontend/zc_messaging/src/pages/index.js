35508f2c8afbe7de39d083221543b086fe61c44a
Tunde Alabi <tundealabi4780@gmail.com>
2021-12-08 13:58:17 +0100

    create the pages/threads folder

--- frontend/zc_messaging/src/pages/index.js
+++ frontend/zc_messaging/src/pages/index.js
@@ -1,5 +1,6 @@
 import DmBrowser from "./dm_browser/DmBrowser"
 import MessagingBoard from "./message-board"
 import ChannelBrowser from "./channel-browser"
+import Threads from "./threads"
 
-export { MessagingBoard, DmBrowser, ChannelBrowser }
+export { MessagingBoard, DmBrowser, ChannelBrowser, Threads }
