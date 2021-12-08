b537af6b8737bda6f03696d9b01c9a514f33638f
Michael Rowland <54159746+mykie88@users.noreply.github.com>
2021-12-08 18:11:52 +0100

    Add to room endpoint (#29)
    * installed dependencies
    
    * add member to a room
    
    * added changes from pr review
    
    * pr review changes on add to room endpoint
    
    * updated docstrings and status codes
    
    * updated return value of get room

--- backend/endpoints/rooms.py
+++ backend/endpoints/rooms.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, BackgroundTasks, HTTPException, status
 from fastapi.responses import JSONResponse
 from schema.response import ResponseModel
-from schema.room import Role, Room, RoomRequest
+from schema.room import AddToRoom, Role, Room, RoomRequest, RoomType
+from utils.centrifugo import Events, centrifugo_client
 from utils.db import DataStorage
-from utils.room_utils import ROOM_COLLECTION
+from utils.room_utils import ROOM_COLLECTION, get_room
 from utils.sidebar import sidebar
 
 router = APIRouter()
@@ -70,4 +71,96 @@ async def create_room(
     raise HTTPException(
         status_code=status.HTTP_424_FAILED_DEPENDENCY,
         detail="unable to create room",
+    )
+
+
+@router.put(
+    "/org/{org_id}/rooms/{room_id}/members/{member_id}",
+    status_code=status.HTTP_200_OK,
+    responses={
+        400: {"detail": "the max number for a Group_DM is 9"},
+        401: {"detail": "member not an admin"},
+        403: {"detail": "DM room or not found"},
+        424: {"detail": "failed to add new members to room"},
+    },
+)
+async def add_to_room(
+    org_id: str,
+    room_id: str,
+    member_id: str,
+    data: AddToRoom,
+    background_tasks: BackgroundTasks,
+):
+    """Adds a new member(s) to a room
+    Args:
+        data: a pydantic schema that defines the request params
+        org_id (str): A unique identifier of an organisation
+        room_id: A unique identifier of the room to be updated
+        member_id: A unique identifier of the member initiating the request
+        background_tasks: A parameter that allows tasks to be performed outside of the main function
+
+    Returns:
+        HTTP_200_OK: {
+                        "status": 200,
+                        "message": "success",
+                        "data": {
+                            "matched_documents": 1,
+                            "modified_documents": 1
+                        }
+                    }
+    Raises:
+        HTTP_400_BAD_REQUEST: the max number for a Group_DM is 9
+        HTTP_401_UNAUTHORIZED: member not in room or not an admin
+        HTTP_403_FORBIDDEN: DM room or not found
+        HTTP_424_FAILED_DEPENDENCY: failed to add new members to room
+    """
+
+    DB = DataStorage(org_id)  # initializes the datastorage class with the org id
+    new_member = data.dict().get("new_member")
+    room = await get_room(org_id=org_id, room_id=room_id)
+
+    if not room or room["room_type"] == RoomType.DM:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="DM room or not found",
+        )
+
+    member = room.get("room_members").get(str(member_id))
+    if member is None or member["role"] != Role.ADMIN:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="member not in room or not an admin",
+        )
+
+    if room["room_type"] == RoomType.CHANNEL:
+        room["room_members"].update(new_member)
+
+    if room["room_type"] == RoomType.GROUP_DM:
+        room["room_members"].update(new_member)
+        if len(room["room_members"].keys()) > 9:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="the max number for a Group_DM is 9",
+            )
+
+    update_members = {"room_members": room["room_members"]}
+    update_response = await DB.update(
+        ROOM_COLLECTION, document_id=room_id, data=update_members
+    )  # updates the room data in the db collection
+
+    background_tasks.add_task(
+        centrifugo_client.publish,
+        room=room_id,
+        event=Events.ROOM_MEMBER_ADD,
+        data=new_member,
+    )  # publish to centrifugo in the background
+
+    if update_response and update_response.get("status_code", None) is None:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=update_response,
+        )
+    raise HTTPException(
+        status_code=status.HTTP_424_FAILED_DEPENDENCY,
+        detail="failed to add new members to room",
     )
--- backend/schema/room.py
+++ backend/schema/room.py
@@ -48,7 +48,7 @@ class RoomMember(BaseModel):
     content of the room_member variable
     """
 
-    role: Role
+    role: Role = Role.MEMBER
     starred: bool = False
     closed: Optional[bool] = False
 
@@ -232,3 +232,9 @@ class Room(RoomRequest):
             )
 
         return values
+
+
+class AddToRoom(BaseModel):
+    """a schema that defines the request params for adding members to a room"""
+
+    new_member: Dict[str, RoomMember]
