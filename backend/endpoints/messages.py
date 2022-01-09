03b8c45e0425421fb2d9995d5a89027390a7661b
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-01-09 22:00:15 +0100

    complete test for update message endpoint

--- backend/endpoints/messages.py
+++ backend/endpoints/messages.py
@@ -175,7 +175,6 @@ async def update_message(
     """
     DB = DataStorage(org_id)
     message = await get_message(org_id, room_id, message_id)
-
     if not message:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Message not found"
