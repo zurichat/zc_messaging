a51e2806f994f0a05bb1107d9905f70bda0d3ec4
kinglighthill <kingholyhill@gmail.com>
2022-01-22 17:58:25 +0100

    added test for remove room member

--- backend/tests/endpoints/conftest.py
+++ backend/tests/endpoints/conftest.py
@@ -79,3 +79,47 @@ def fixture_mock_dataStorage_delete(mocker):
     zc_core_update_data = AsyncMock()
     mocker.patch("utils.db.DataStorage.delete", side_effect=zc_core_update_data)
     return zc_core_update_data
+
+
+@pytest.fixture(name="init_fake_room")
+def fixture_initialize_fake_room_data():
+    """A fixture that initialises a new fake room data for each test
+
+    Returns:
+        Dict: An room dictionary
+    """
+    room_data = {
+        "room_name": "General",
+        "room_type": "CHANNEL",
+        "room_members": {
+            "61696f5ac4133ddaa309dcfe": {
+                "closed": False,
+                "role": "admin",
+                "starred": False,
+            },
+            "6169704bc4133ddaa309dd07": {
+                "closed": False,
+                "role": "admin",
+                "starred": False,
+            },
+            "619baa5c1a5f54782939d386": {
+                "closed": False,
+                "role": "member",
+                "starred": False,
+            },
+            "619baa5c1a5f54782930d386": {
+                "closed": False,
+                "role": "member",
+                "starred": True,
+            },
+        },
+        "created_at": "2022-01-11 03:18:02.364291",
+        "description": None,
+        "topic": "General Information",
+        "is_private": False,
+        "is_archived": False,
+        "id": "23dg67l0eba8adb50ca13a24",
+        "org_id": "3467sd4671a5f5478df56u911",
+        "created_by": "619ba4671a5f54782939d385",
+    }
+    return room_data
--- backend/tests/endpoints/test_room.py
+++ backend/tests/endpoints/test_room.py
@@ -82,6 +82,31 @@ join_room_success_response = {
     },
 }
 
+leave_room_url = (
+    "api/v1/org/3467sd4671a5f5478df56u911/rooms/23dg67l0eba8adb50ca13a24/"
+    + "members/619baa5c1a5f54782939d386"
+)
+
+remove_room_member_url = (
+    "api/v1/org/3467sd4671a5f5478df56u911/rooms/23dg67l0eba8adb50ca13a24/"
+    + "members/61696f5ac4133ddaa309dcfe?admin_id=6169704bc4133ddaa309dd07"
+)
+
+fake_member_leave_room_url = (
+    "api/v1/org/3467sd4671a5f5478df56u911/rooms/23dg67l0eba8adb50ca13a24/"
+    + "members/1696f5ac4133ddaa309dcfe?admin_id=6169704bc4133ddaa309dd07"
+)
+
+fake_member_remove_room_member_url = (
+    "api/v1/org/3467sd4671a5f5478df56u911/rooms/23dg67l0eba8adb50ca13a24/"
+    + "members/1696f5ac4133ddaa309dcfe"
+)
+
+non_admin_remove_room_member_url = (
+    "api/v1/org/3467sd4671a5f5478df56u911/rooms/23dg67l0eba8adb50ca13a24/"
+    + "members/61696f5ac4133ddaa309dcfe?admin_id=619baa5c1a5f54782939d386"
+)
+
 
 @pytest.mark.asyncio
 @mock.patch.object(DataStorage, "__init__", lambda x, y: None)
@@ -348,3 +373,152 @@ async def test_get_members_unsuccessful(mock_dataStorage_read):
     response = client.get(url=get_room_members_url)
     assert response.status_code == 424
     assert response.json() == {"detail": "Failure to retrieve room members"}
+
+
+@pytest.mark.parametrize(
+    "url, status_code, json_response",
+    [
+        (
+            leave_room_url,
+            200,
+            {
+                "status": "success",
+                "message": "user removed from room successfully",
+                "data": {
+                    "matched_documents": 1,
+                    "modified_documents": 1,
+                    "member_id": "619baa5c1a5f54782939d386",
+                    "room_id": "23dg67l0eba8adb50ca13a24",
+                },
+            },
+        ),
+        (
+            remove_room_member_url,
+            200,
+            {
+                "status": "success",
+                "message": "user removed from room successfully",
+                "data": {
+                    "matched_documents": 1,
+                    "modified_documents": 1,
+                    "member_id": "61696f5ac4133ddaa309dcfe",
+                    "room_id": "23dg67l0eba8adb50ca13a24",
+                },
+            },
+        ),
+        (
+            f"{fake_member_leave_room_url}",
+            404,
+            {"detail": "user not a member of the room"},
+        ),
+        (
+            f"{fake_member_remove_room_member_url}",
+            404,
+            {"detail": "user not a member of the room"},
+        ),
+        (
+            f"{remove_room_member_url}90",
+            404,
+            {"detail": "admin id specified not a member of the room"},
+        ),
+        (
+            f"{non_admin_remove_room_member_url}",
+            403,
+            {"detail": "must be an admin to remove member"},
+        ),
+    ],
+)
+@pytest.mark.asyncio
+@mock.patch.object(DataStorage, "__init__", lambda x, y: None)
+async def test_remove_room_member(
+    init_fake_room,
+    mock_dataStorage_read,
+    mock_dataStorage_update,
+    url,
+    status_code,
+    json_response,
+):
+    """
+    Leave room successfully
+    Args:
+        mock_datastorage_read (AyncMock): Asynchronous external api call
+    """
+
+    db = DataStorage("3467sd4671a5f5478df56u911")
+    db.plugin_id = "34453"
+
+    update_response = {
+        "status": 200,
+        "message": "success",
+        "data": {"matched_documents": 1, "modified_documents": 1},
+    }
+
+    mock_dataStorage_read.return_value = init_fake_room
+    mock_dataStorage_update.return_value = update_response
+
+    response = client.patch(url=url)
+
+    assert response.status_code == status_code
+    assert response.json() == json_response
+
+
+@pytest.mark.asyncio
+@mock.patch.object(DataStorage, "__init__", lambda x, y: None)
+async def test_remove_room_member_wrong_room_id(
+    mock_dataStorage_read, mock_dataStorage_update
+):
+    """
+    Leave room successfully
+    Args:
+        mock_datastorage_read (AyncMock): Asynchronous external api call
+    """
+
+    db = DataStorage("3467sd4671a5f5478df56u911")
+    db.plugin_id = "34453"
+
+    update_response = {
+        "status": 200,
+        "message": "success",
+        "data": {"matched_documents": 1, "modified_documents": 1},
+    }
+
+    mock_dataStorage_read.return_value = {}
+    mock_dataStorage_update.return_value = update_response
+
+    response = client.patch(url=remove_room_member_url)
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "room does not exist"}
+
+
+@pytest.mark.asyncio
+@mock.patch.object(DataStorage, "__init__", lambda x, y: None)
+async def test_remove_room_member_from_dm(
+    init_fake_room, mock_dataStorage_read, mock_dataStorage_update
+):
+    """
+    Leave room successfully
+    Args:
+        mock_datastorage_read (AyncMock): Asynchronous external api call
+    """
+
+    db = DataStorage("3467sd4671a5f5478df56u911")
+    db.plugin_id = "34453"
+
+    init_fake_room["room_type"] = "DM"
+
+    update_response = {
+        "status": 200,
+        "message": "success",
+        "data": {"matched_documents": 1, "modified_documents": 1},
+    }
+
+    mock_dataStorage_read.return_value = init_fake_room
+    mock_dataStorage_update.return_value = update_response
+
+    response = client.patch(url=remove_room_member_url)
+
+    print(response.json())
+
+    assert response.status_code == 403
+    assert response.json() == {"detail": "cannot remove member from DM rooms"}
