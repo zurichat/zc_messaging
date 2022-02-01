393db6671bf46fa428e314de14e3670b71efef08
xzenon <forlanfr_8@hotmail.fr>
2022-02-01 19:09:11 +0100

    Complete docstring for get_message

--- backend/utils/message_utils.py
+++ backend/utils/message_utils.py
@@ -80,15 +80,35 @@ async def get_room_messages(
     return response
 
 
-async def get_message(org_id: str, room_id: str, message_id: str) -> dict:
-    """Get messages in a room
+async def get_message(
+    org_id: str, room_id: str, message_id: str
+) -> Optional[dict[str, Any]]:
+    """Get a specific message in a room.
+
     Args:
         org_id (str): The organization id
-        room_id (str): The room id
-        message_id (str): The message id
+        room_id (str): The id of the room where to the message is to be retrieved.
+        message_id (str): The id of the message to be retrieved.
+
     Returns:
-        dict: key value pair of message info mapped according to message schema
+        dict: key value pair of the message info mapped according to message schema.
+
+        {
+                        "_id": "61eff23565934b58b8e5d26f",
+                        "created_at": "2022-01-24 18:05:01.129393",
+                        "edited": false,
+                        "emojis": [],
+                        "files": [],
+                        "org_id": "619ba4671a5f54782939d384",
+            ...,
+                        "room_id": "61e72d1265934b58b8e5d220",
+                        "saved_by": [],
+                        "sender_id": "61e59f1b65934b58b8e5d1cb",
+                        "threads": [],
+                        "timestamp": 0
+                },
     """
+
     DB = DataStorage(org_id)
     query = {"room_id": room_id, "_id": message_id}
     response = await DB.read(settings.MESSAGE_COLLECTION, query=query)
