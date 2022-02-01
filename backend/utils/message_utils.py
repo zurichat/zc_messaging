2a035f819ff00b3e295c09986fc1c5a17594b390
xzenon <forlanfr_8@hotmail.fr>
2022-02-01 17:41:16 +0100

    Complete docstring for get_org_messages function

--- backend/utils/message_utils.py
+++ backend/utils/message_utils.py
@@ -1,21 +1,43 @@
+from typing import Any, Optional
+
 from config.settings import settings
 from utils.db import DataStorage
 
 
-async def get_org_messages(org_id: str) -> list:
-    """Get all messages in a room
+async def get_org_messages(org_id: str) -> Optional[list[dict[str, Any]]]:
+    """Gets all messages sent in  an organization.
+
     Args:
         org_id (str): The organization id
 
     Returns:
-        dict: key value pair of message info mapped according to message schema
+        list[dict]: A list of key value pairs of messages info mapped according to message schema.
+
+        {
+            "status": 200,
+            "message": "success",
+            "data": [
+                {
+                    "_id": "61e6878165934b58b8e5d1e0",
+                    "created_at": "2022-01-18 09:05:32.479911",
+                    "edited": false,
+                    ...
+                },
+                {
+                    "_id": "61e6878165934b58b8e5d1e1",
+                    "created_at": "2022-01-18 09:05:32.479911",
+                    "edited": true,
+                    ...
+                },
+                ...
+        }
     """
 
     DB = DataStorage(org_id)
-    response = await DB.read(settings.MESSAGE_COLLECTION, {})
+    response = await DB.read(settings.MESSAGE_COLLECTION)
     if response and "status_code" not in response:
         return response
-    return {}
+    return None
 
 
 async def get_room_messages(org_id: str, room_id: str) -> list:
