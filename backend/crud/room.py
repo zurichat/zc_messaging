a74b5fa9ec6ea5aab18129170c36524c567d1fc2
xzenon <forlanfr_8@hotmail.fr>
2022-03-26 18:45:08 +0100

    Create crud function update_room

--- backend/crud/room.py
+++ backend/crud/room.py
@@ -17,3 +17,22 @@ async def add_room(org_id: str, room: Room) -> dict[str, Any]:
     db = DataStorage(org_id)
 
     return await db.write(settings.ROOM_COLLECTION, room.dict())
+
+
+async def update_room(
+    org_id: str, room_id: str, data: dict[str, Any]
+) -> dict[str, Any]:
+    """Updates a room document in the database.
+
+    Args:
+        org_id (str): The organization id where the room is being updated.
+        room_id (str): The id of the room to be edited.
+        room (dict[str, Any]): The new data.
+
+    Returns:
+        dict[str, Any]: The response returned by DataStorage's update method.
+    """
+
+    db = DataStorage(org_id)
+
+    return await db.update(settings.ROOM_COLLECTION, room_id, data)
