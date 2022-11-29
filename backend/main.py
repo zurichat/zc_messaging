92731f32e13b8f92045068582a46f5035eaacd8f
Chukwuedo Bernadette <100119269+Bernadettechukwuedo@users.noreply.github.com>
2022-11-29 20:13:59 +0100

    corrected the endpoint for getting the details of a file (#166)
    * corrected the endpoint for getting the details of a file
    
    * corrected the endpoint for getting the details of a file
    
    * fix: made some changes
    
    * fix: merge conflict
    
    * fix: made some changes
    
    * fix: made some changes
    
    * fix: fixed the return statement
    
    * fix: fixed errors in details_of_file endpoint
    
    * fix: deleted unused files

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from config.settings import settings
-from endpoints import members, messages, rooms, sync, threads, files
+from endpoints import members, messages, rooms, sync, threads, files, detail_file
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
@@ -34,7 +34,10 @@ app.include_router(
     sync.router, prefix=settings.API_V1_STR, tags=["sync"]
 )  # include urls from sync.py
 app.include_router(
-    files.router, prefix=settings.API_V1_STR, tags=["files"]
+    detail_file.router, prefix=settings.API_V1_STR, tags=["details_of_files"]
+)  # include urls from detail_file.py
+app.include_router(
+        files.router, prefix=settings.API_V1_STR, tags=["files"]
 )   # include urls from files.py
 
 app.mount(
