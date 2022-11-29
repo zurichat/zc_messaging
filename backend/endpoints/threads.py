5aa15b35129e2bbbd0f8cd8602ddb12d6054f8f1
Harrylever <ukanah15thdean@gmail.com>
2022-11-29 12:16:01 +0100

    Remove backend changes from Frontend commit

--- backend/endpoints/threads.py
+++ backend/endpoints/threads.py
@@ -4,7 +4,7 @@ from schema.message import Thread, MessageRequest, Message
 from schema.response import ResponseModel
 from fastapi.responses import JSONResponse
 from utils.centrifugo import Events, centrifugo_client
-from utils.message_utils import get_message, get_room_messages, update_message_threads as edit_message_threads
+from utils.message_utils import get_message, get_room_messages
 from utils.room_utils import get_org_rooms
 from utils.threads_utils import get_messages_in_thread, add_message_to_thread
 import json
@@ -136,103 +136,3 @@ async def get_thread_messages(org_id: str, room_id: str, message_id: str):
         status_code=status.HTTP_200_OK,
     )
 
-@router.put(
-		"/org/{org_id}/rooms/{room_id}/messages/{message_id}/edithreads",
-		response_model=ResponseModel,
-		status_code=status.HTTP_200_OK,
-		responses={
-				401: {"description": "You are not authorized to edit this message"},
-				404: {"description": "Message not found"},
-				424: {"description": "Message not edited"},
-		},
-)
-async def update_threads_message(
-		org_id: str,
-		room_id: str,
-		message_id: str,
-		request: MessageRequest,
-		background_tasks: BackgroundTasks,
-):
-
-		payload = request.dict()
-		loadedMessage = await get_message(org_id, room_id, message_id)
-		loadedMessageThread = loadedMessage['threads']
-		thread_to_add = {"threads": [payload, *loadedMessageThread]}
-
-		if not loadedMessage:
-				raise HTTPException(
-						status_code=status.HTTP_404_NOT_FOUND, detail="Message not found"
-				)
-
-		added_thread_message = await edit_message_threads(org_id, message_id, thread_to_add)
-
-		if not added_thread_message:
-				raise HTTPException(
-						status_code=status.HTTP_424_FAILED_DEPENDENCY,
-						detail={"thread not added": added_thread_message},
-				)
-
-		# payload["edited"] = True
-		loadedMessage.update(thread_to_add)
-
-		# Publish to centrifugo in the background.
-		background_tasks.add_task(
-				centrifugo_client.publish,
-				room_id, 
-				Events.MESSAGE_UPDATE, 
-				loadedMessage
-		)
-
-		return JSONResponse(
-				content=ResponseModel.success(data=loadedMessage, message="Thread Added"),
-				status_code=status.HTTP_200_OK,
-		)
-
-@router.get(
-		"/org/{org_id}/member/{member_id}/threads",
-		response_model=ResponseModel,
-		status_code=status.HTTP_200_OK,
-		responses={424: {"detail": "ZC Core failed"}},
-)
-async def get_threads(org_id: str, member_id: str):
-		room_response = await get_org_rooms(org_id=org_id, member_id=member_id)
-
-		if room_response == []:
-			raise HTTPException(
-				status_code=status.HTTP_404_NOT_FOUND,
-				detail="Member does not exist or Org not found"
-			)
-
-		if room_response is None:
-			raise HTTPException(
-				status_code=status.HTTP_424_FAILED_DEPENDENCY,
-				detail="Zc Core failed"
-			)
-
-		room_ids = []
-
-		for room in room_response:
-			room_ids.append(room['_id'])
-		
-		for room_id in room_ids:
-			single_room_resp = await get_room_messages(org_id, room_id)
-			if single_room_resp == []:
-					# Room not found
-					pass
-
-			if single_room_resp is None:
-					# Zc Core failed
-					pass
-
-			# Gotten Single Room
-
-		messages_w_thread = []
-
-		for messages in single_room_resp:
-			if messages['threads'] != []:
-				messages_w_thread.append(messages)
-
-		return JSONResponse(
-				content=ResponseModel.success(data=messages_w_thread, message="Rooms retrieved"),
-				status_code=status.HTTP_200_OK,
-		)
