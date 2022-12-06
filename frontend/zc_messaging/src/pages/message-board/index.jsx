5f66455289bb3fb02c17438dbe23dba15aa6509e
Taiwo Sulaiman <tsulaimon96@gmail.com>
2022-12-06 19:06:57 +0100

    Feature/message thread (#184)
    * fix reflect send message
    
    * fix: messaging thread and emojis
    
    Co-authored-by: ibimina <96787210+ibimina@users.noreply.github.com>

--- frontend/zc_messaging/src/pages/message-board/index.jsx
+++ frontend/zc_messaging/src/pages/message-board/index.jsx
@@ -3,7 +3,12 @@ import { useNavigate, useParams, Outlet } from "react-router-dom"
 import { Helmet } from "react-helmet"
 import { MessageBoard, MessageRoomViewHeader } from "@zuri/ui"
 import { subscribeToChannel } from "@zuri/utilities"
-import { Container, MessagingArea, TypingNotice } from "./MessageBoard.style"
+import {
+  Container,
+  MessagingArea,
+  TypingNotice,
+  MessageWrapper
+} from "./MessageBoard.style"
 import fetchDefaultRoom from "../../utils/fetchDefaultRoom"
 import { useSelector, useDispatch } from "react-redux"
 import getMessageSender from "../../utils/getMessageSender.js"
@@ -26,7 +31,11 @@ const MessagingBoard = () => {
   const [roomName, setRoomName] = useState("unknown-channel")
   const [pageIndex, setPageIndex] = useState(1)
   const [roomChats, setRoomChats] = useState([])
-  const [saveRecentChat, setSaveRecentChat] = useState([])
+  const [showEmoji, setShowEmoji] = useState(false)
+  const [isProcessing, setIsProcessing] = useState({
+    status: false,
+    message: []
+  })
   const chatSize = 15
   const { data: roomsAvailable, isLoading: IsLoadingRoomsAvailable } =
     useGetRoomsAvailableToUserQuery(
@@ -103,6 +112,17 @@ const MessagingBoard = () => {
       }
     }
   }, [roomId, roomsAvailable])
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+  function generateString() {
+    let result = " "
+    const charactersLength = characters.length
+    for (let i = 0; i < 10; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
+  }
 
   const sendMessageHandler = async message => {
     const currentDate = new Date()
@@ -111,6 +131,20 @@ const MessagingBoard = () => {
       emojis: [],
       richUiData: message
     }
+    const newMessages = [
+      {
+        ...newMessage,
+        _id: generateString(),
+        orgId: currentWorkspaceId,
+        roomId,
+        sender: {
+          sender_id: authUser?.user_id,
+          sender_name: authUser?.user_name,
+          sender_image_url: authUser?.user_image_url
+        }
+      }
+    ]
+    setIsProcessing({ status: true, message: roomChats.concat(newMessages) })
     sendNewMessage({
       orgId: currentWorkspaceId,
       roomId,
@@ -121,21 +155,27 @@ const MessagingBoard = () => {
       },
       messageData: { ...newMessage }
     })
-    const newMessages = [
-      {
-        ...newMessage,
-        orgId: currentWorkspaceId,
-        roomId,
-        sender: {
-          sender_id: authUser?.user_id,
-          sender_name: authUser?.user_name,
-          sender_image_url: authUser?.user_image_url
-        }
-      }
-    ]
-    setPageIndex(1)
-    setSaveRecentChat(prev => prev.concat(newMessages))
-    setRoomChats(prev => prev.concat(saveRecentChat))
+      .then(e => {
+        const newMessages = [
+          {
+            ...newMessage,
+            _id: e.data.data.message_id,
+            orgId: currentWorkspaceId,
+            roomId,
+            sender: {
+              sender_id: authUser?.user_id,
+              sender_name: authUser?.user_name,
+              sender_image_url: authUser?.user_image_url
+            }
+          }
+        ]
+        setRoomChats(prev => prev.concat(newMessages))
+        setIsProcessing({ status: false, message: [] })
+      })
+      .catch(() => {
+        setIsProcessing({ status: false, message: [] })
+      })
+
     return true
   }
 
@@ -156,6 +196,7 @@ const MessagingBoard = () => {
     }
 
     const message = newMessages[messageIndex]
+
     const emojiIndex = message.emojis.findIndex(
       emoji => emoji.name.toLowerCase() === newEmojiName.toLowerCase()
     )
@@ -180,6 +221,16 @@ const MessagingBoard = () => {
             edited: true,
             message_id: messageId
           }
+          //   Finds the message ID and updates the emojis array for the particular message
+          const newState = roomChats.map(chat => {
+            if (chat._id === messageId) {
+              return { ...updatedMessage }
+            }
+            return chat
+          })
+          //    sets the message array state
+          setRoomChats(newState)
+          //
           updateMessage({
             orgId: currentWorkspaceId,
             roomId,
@@ -193,13 +244,14 @@ const MessagingBoard = () => {
           })
           // message.emojis.splice(emojiIndex, 1)
         } else {
+          console.log("Block 2")
+
           message.emojis[emojiIndex].reactedUsersId.splice(reactedUserIdIndex)
           message.emojis[emojiIndex].count =
             message.emojis[emojiIndex].count - 1
         }
       } else {
         // the user has not reacted and will now be added to the list and count incremented
-
         message.emojis[emojiIndex].reactedUsersId = [
           ...message.emojis[emojiIndex].reactedUsersId,
           currentUserId
@@ -209,6 +261,7 @@ const MessagingBoard = () => {
     } else {
       // the emoji does not exist
       // create the emoji object and push
+      console.log("Block 1")
 
       const newEmojiObject = {
         name: newEmojiName,
@@ -223,7 +276,16 @@ const MessagingBoard = () => {
         edited: true,
         message_id: messageId
       }
-
+      //   Finds the message ID and updates the emojis array for the particular message
+      const newState = roomChats.map(chat => {
+        if (chat._id === messageId) {
+          return { ...updatedMessage }
+        }
+        return chat
+      })
+      //    sets the message array state
+      setRoomChats(newState)
+      //
       updateMessage({
         orgId: currentWorkspaceId,
         roomId,
@@ -236,41 +298,54 @@ const MessagingBoard = () => {
         messageId: updatedMessage._id
       })
     }
-
+    setShowEmoji(false)
     return false
   }
 
   const SendAttachedFileHandler = file => {
     // do something with the file
   }
+
   const handleScroll = event => {
     const numPage = Math.ceil(data.total / chatSize)
     if (event.currentTarget.scrollTop === 0 && pageIndex < numPage) {
       setPageIndex(prev => prev + 1)
-      setRoomChats(prev => data.roomMessages.concat(prev))
-      event.currentTarget.scrollTop = 200
     }
   }
+  useEffect(() => {
+    if (data?.roomMessages.length) {
+      setRoomChats(prev => data.roomMessages.concat(prev))
+    }
+  }, [data?.roomMessages])
+
+  //
+  //
   return roomId ? (
     <>
       <Helmet>
         <title>{pageTitle}</title>
       </Helmet>
-      <MessageRoomViewHeader name={`#${roomName}`} />
       <Container>
         <MessagingArea>
-          <div style={{ height: "100%" }}>
+          <MessageWrapper>
+            <MessageRoomViewHeader name={`#${roomName}`} />
             <MessageBoard
               isLoadingMessages={isLoadingRoomMessages}
-              messages={roomChats || []}
+              messages={
+                (isProcessing.status === true
+                  ? isProcessing.message
+                  : roomChats) || []
+              }
               onSendMessage={sendMessageHandler}
               onReact={reactHandler}
               onSendAttachedFile={SendAttachedFileHandler}
               currentUserId={authUser?.user_id}
               height={"92vh"}
               onHandleScroll={handleScroll}
+              showEmoji={showEmoji}
+              setShowEmoji={setShowEmoji}
             />
-          </div>
+          </MessageWrapper>
           {/* <TypingNotice>Omo Jesu is typing</TypingNotice> */}
         </MessagingArea>
 
